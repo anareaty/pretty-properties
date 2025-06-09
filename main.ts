fc6658cf4a2e3119f4b07d88e64632e28f67e43a
anareaty <reatyrabota@yandex.ru>
2025-06-09 19:06:02 +0400

    release

--- main.ts
+++ main.ts
@@ -1,86 +1,532 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, TextFileView , FileView , EditableFileView , CachedMetadata, MarkdownRenderer, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile, MenuItem } from 'obsidian';
+
+
+import MenuManager from 'MenuManager';
+
+
+
+
+
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
 	mySetting: string;
+	hiddenProperties: string[];
+	propertyPillColors: any
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	mySetting: 'default',
+	hiddenProperties: [],
+	propertyPillColors: {}
 }
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
+	menuManager: any
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		this.updateHiddenProperties()
+		this.updatePillColors()
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+
+		this.registerEvent(
+			this.app.workspace.on("layout-change", async () => {
+				this.updateSideImages(null, null)
+				this.updateBanners(null, null)
+				this.addClassestoProperties()
+			})
+		);
+
+
+		this.registerEvent(
+			this.app.workspace.on("file-open", async (file) => {
+				this.updateSideImages(file, null);
+				this.updateBanners(file, null);
+				this.addClassestoProperties();
+			})
+		);
+
+		
+
+		this.registerEvent(
+			this.app.metadataCache.on("changed", async (file, data, cache) => {
+				this.updateSideImages(file, cache)
+				this.updateBanners(file, cache)
+				this.addClassestoProperties()
+			})
+		);
+
+
+
+		//@ts-ignore
+		if (this.app.isMobile) {
+			this.registerDomEvent(document, "contextmenu", (e: MouseEvent) => {
+				if (e.target instanceof HTMLElement && 
+				e.target.closest(".multi-select-pill")) {
+				  this.handlePillMenu(e.target)
 				}
-			}
-		});
+			});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
+
+			this.registerDomEvent(document, "touchstart", (e: TouchEvent) => {
+				if ((e.target instanceof HTMLElement || 
+				e.target instanceof SVGElement) && 
+				e.target.closest(".metadata-property-icon")) {
+				  this.handlePropertyMenu(e.target)
+				}
+			});
+
+
+
+		} else {
+			this.registerDomEvent(document, "mousedown", (e: MouseEvent) => {
+				if (e.button == 2 && e.target instanceof HTMLElement && 
+				  e.target.closest(".multi-select-pill")) {
+					this.handlePillMenu(e.target)
+				}
+				
+				if ((e.target instanceof HTMLElement || 
+				  e.target instanceof SVGElement) && 
+				  e.target.closest(".metadata-property-icon")) {
+					this.handlePropertyMenu(e.target)
+				}
+			  });
+		}
+
+
+
+
+		this.addCommand({
+            id: "toggle-hidden-properties",
+            name: "Показать / спрятать скрытые свойства",
+            callback: async () => {
+                document.body.classList.toggle("show-hidden-properties")
+            }
+        })
+	
+
+
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
 
 	}
+
+	addCommandToPropertyMenu(e: MouseEvent) {
+		if (e.target instanceof HTMLElement || e.target instanceof SVGElement) {
+			e.stopPropagation()
+		let propEl = e.target.closest(".metadata-property")
+
+		
+		}
+		
+	}
+
+	addCommandToPillMenu(e: MouseEvent) {}
+
+
+
+
+	handlePropertyMenu(el: HTMLElement | SVGElement) {
+		let propEl = el.closest(".metadata-property");
+		if (propEl instanceof HTMLElement) {
+			let propName = propEl?.getAttribute("data-property-key")
+
+			if (propName) {
+
+				let menuManager = new MenuManager()
+
+				if (this.settings.hiddenProperties.find(p => p == propName)) {
+
+					menuManager.addItemAfter(['clipboard'], (item: MenuItem) => item
+						.setTitle("Не скрывать свойство")
+						.setIcon('lucide-image-plus')
+						.setSection('pretty-properties')
+						.onClick(() => {
+							this.settings.hiddenProperties.remove(propName)
+							this.saveSettings()
+							this.updateHiddenProperties()
+						})
+					);
+
+				} else {
+
+					menuManager.addItemAfter(['clipboard'], (item: MenuItem) => item
+						.setTitle("Скрыть свойство")
+						.setIcon('lucide-image-plus')
+						.setSection('pretty-properties')
+						.onClick(() => {
+							this.settings.hiddenProperties.push(propName)
+							this.saveSettings()
+							this.updateHiddenProperties()
+						})
+					);
+					
+				}
+				
+			}
+		}
+	}
+
+
+
+	handlePillMenu(el: HTMLElement) {
+		let menuManager = new MenuManager();
+		let pillEl = el.closest(".multi-select-pill");
+		if (pillEl instanceof HTMLElement) {
+			let pillVal = pillEl?.getAttribute("data-property-pill-value")
+
+			if (pillVal) {
+				menuManager.addItemAfter(['clipboard'], (item: MenuItem) => {
+					item.setTitle("Выбрать цвет")
+					.setSection('pretty-properties')
+
+					//@ts-ignore
+					let sub = item.setSubmenu() as Menu
+					let colors = ["red", "orange", "yellow", "green", "cyan", "blue", "purple", "pink", "none"]
+
+					for (let color of colors) {
+						sub.addItem((item: MenuItem) => {
+						item.setIcon("square")
+
+						if (color != "default" && color != "none") {
+							//@ts-ignore
+							item.iconEl.style = "color: transparent; background-color: rgba(var(--color-" + color + "-rgb), 0.3);"
+						}
+
+						if (color == "none") {
+							//@ts-ignore
+							item.iconEl.style = "opacity: 0.2;"
+						}
+
+
+						item.setTitle(color)
+						.onClick(() => {
+							this.settings.propertyPillColors[pillVal] = color
+							this.saveSettings()
+							this.updatePillColors()
+						})
+					})
+					}
+				});
+			}
+		}
+	}
+
+
+
+	addCommandsToMenus(e: MouseEvent) {
+		if ((e.target instanceof HTMLElement || e.target instanceof SVGElement) && 
+			e.target.closest(".metadata-property-key")) {
+			
+		}
+
+		if (e.target instanceof HTMLElement && e.target.closest(".multi-select-pill")) {
+			e.stopPropagation()
+
+			let menuManager = new MenuManager()
+
+			let pillEl = e.target.closest(".multi-select-pill")
+
+			if (pillEl instanceof HTMLElement) {
+				let pillVal = pillEl?.getAttribute("data-property-pill-value")
+
+				if (pillVal) {
+
+					menuManager.addItemAfter(['clipboard'], (item: MenuItem) => {
+						item.setTitle("Выбрать цвет")
+						.setSection('pretty-properties')
+
+						//@ts-ignore
+						let sub = item.setSubmenu() as Menu
+
+						let colors = ["red", "orange", "yellow", "green", "cyan", "blue", "purple", "pink", "none"]
+
+						for (let color of colors) {
+							sub.addItem((item: MenuItem) => {
+							item.setTitle(color)
+							.onClick(() => {
+								this.settings.propertyPillColors[pillVal] = color
+								this.saveSettings()
+								this.updatePillColors()
+							})
+						})
+						}
+
+						
+					}
+
+
+
+						
+					);
+
+				}
+			}
+		
+				
+		}
+	}
+
+
+
+
+	updateHiddenProperties() {
+		let styleText = ""
+		for (let prop of this.settings.hiddenProperties) {
+			styleText = styleText + "body:not(.show-hidden-properties) .metadata-property[data-property-key='" + prop + "'] {display: none;}\n"
+		}
+
+		let oldStyle = document.head.querySelector("style#hide-properties")
+		if (oldStyle) oldStyle.remove()
+
+
+		const style = document.createElement("style")
+		style.textContent = styleText
+		style.id = "hide-properties"
+		document.head.appendChild(style)
+	}
+
+
+	updatePillColors() {
+
+		let styleText = ""
+		for (let prop in this.settings.propertyPillColors) {
+			styleText = styleText + "[data-property-pill-value='" + prop + "'] {" + 
+			"--pill-color-rgb: var(--color-" + this.settings.propertyPillColors[prop] + "-rgb); \n" + 
+			"--pill-background: rgba(var(--pill-color-rgb), 0.2); \n--pill-background-hover: rgba(var(--pill-color-rgb), 0.3);}\n"
+		}
+
+		let oldStyle = document.head.querySelector("style#pill-colors")
+		if (oldStyle) oldStyle.remove()
+
+
+		const style = document.createElement("style")
+		style.textContent = styleText
+		style.id = "pill-colors"
+		document.head.appendChild(style)
+
+	}
+
+
+
+	updateSideImages(changedFile: TFile | null, cache: CachedMetadata | null) {
+
+		let leaves = this.app.workspace.getLeavesOfType("markdown")
+
+		for (let leaf of leaves) {
+
+		if (leaf.view instanceof MarkdownView) {
+			if(changedFile && leaf.view.file && leaf.view.file.path != changedFile.path) {
+				continue
+			}
+	
+			//@ts-ignore
+			let mdEditor = leaf.view.metadataEditor
+			let mdContainer = mdEditor.containerEl
+
+
+			let coverVal
+			let cssVal
+
+			let coverProp = mdEditor.properties.find((p: any) => p.key == "cover")
+			let cssProp = mdEditor.properties.find((p: any) => p.key == "cssclasses")
+
+			if (coverProp) {
+				coverVal = coverProp.value
+				if (cssProp) {
+					cssVal = cssProp.value
+				}
+			} else {
+				if (leaf.view.file instanceof TFile) {
+					let cache = this.app.metadataCache.getFileCache(leaf.view.file)
+					coverVal = cache?.frontmatter?.cover
+					cssVal = cache?.frontmatter?.cssclasses
+				}
+			}
+
+			let coverDiv
+			let oldCoverDiv = mdContainer.querySelector(".metadata-side-image")
+
+			if (coverVal) {
+				coverDiv = document.createElement("div");
+				coverDiv.classList.add("metadata-side-image")
+			
+				if (cssVal && cssVal.includes("cover-vertical")) {
+				coverDiv.classList.add("vertical")
+				}
+	
+				else if (cssVal && cssVal.includes("cover-horizontal")) {
+				coverDiv.classList.add("horizontal")
+				}
+	
+				else if (cssVal && cssVal.includes("cover-square")) {
+				coverDiv.classList.add("square")
+				}
+
+				else if (cssVal && cssVal.includes("cover-circle")) {
+				coverDiv.classList.add("circle")
+				}
+	
+				else {
+				coverDiv.classList.add("vertical")
+				}
+	
+				let coverTemp = document.createElement("div");
+				MarkdownRenderer.render(this.app, coverVal, coverTemp, "", this);
+				let image = coverTemp.querySelector("img")
+				if (image) {
+					coverDiv.append(image)
+				}
+			}
+			
+	
+			if (coverDiv) {
+			if (oldCoverDiv) {
+				if (coverDiv.outerHTML != oldCoverDiv.outerHTML) {
+				oldCoverDiv.remove()
+				mdContainer.prepend(coverDiv)
+				}
+			} else {
+				mdContainer.prepend(coverDiv)
+			}
+			} else {
+			if (oldCoverDiv) oldCoverDiv.remove()
+			}
+		}
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	updateBanners(changedFile: TFile | null, cache: CachedMetadata | null) {
+		
+		let leaves = this.app.workspace.getLeavesOfType("markdown")
+
+		for (let leaf of leaves) {
+
+			if (leaf.view instanceof MarkdownView) {
+				if(changedFile && leaf.view.file && leaf.view.file.path != changedFile.path) {
+					continue
+				}
+
+				let contentEl = leaf.view.contentEl
+
+				//@ts-ignore
+				let mdEditor = leaf.view.metadataEditor
+				let mdContainer = mdEditor.containerEl
+				let mode = leaf.view.getMode()
+
+				if (mode == "preview") {
+					mdContainer = contentEl.querySelector(".markdown-preview-view")
+				}
+
+				if (mode == "source") {
+					mdContainer = contentEl.querySelector(".cm-scroller")
+				}
+
+
+				let bannerVal
+
+				let bannerProp = mdEditor.properties.find((p: any) => p.key == "banner")
+
+				if (bannerProp) {
+					bannerVal = bannerProp.value
+				} else {
+					if (leaf.view.file instanceof TFile) {
+						let cache = this.app.metadataCache.getFileCache(leaf.view.file)
+						bannerVal = cache?.frontmatter?.banner
+					}
+				}
+
+				let bannerDiv = document.createElement("div");
+				bannerDiv.classList.add("banner-image")
+
+				let oldBannerDiv = mdContainer.querySelector(".banner-image")
+				
+
+				if (bannerVal) {
+					let bannerTemp = document.createElement("div");
+					MarkdownRenderer.render(this.app, bannerVal, bannerTemp, "", this);
+					let image = bannerTemp.querySelector("img")
+					if (image) {
+						bannerDiv.append(image)
+					}
+				}
+				
+				if (oldBannerDiv) {
+					if (oldBannerDiv.outerHTML != bannerDiv.outerHTML) {
+						oldBannerDiv.remove();
+						mdContainer.prepend(bannerDiv);
+					}
+				} else {
+					mdContainer.prepend(bannerDiv)
+				}
+
+			}
+		}
+	}
+
+
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+		addClassestoProperties() {
+		  let leaves = this.app.workspace.getLeavesOfType("markdown")
+		  for (let leaf of leaves) {
+
+			//@ts-ignore
+			let mdEditor = leaf.view.metadataEditor;
+      		let container = mdEditor.containerEl;
+
+			let pills = container.querySelectorAll(".multi-select-pill:not([data-property-pill-value])")
+			for (let pill of pills) {
+			  let content = pill.querySelector(".multi-select-pill-content")
+			  if (content instanceof HTMLElement) {
+				let value = content.innerText
+			  	pill.setAttribute("data-property-pill-value", value)
+			  }
+			}
+		  }
+		}
+
+
+
+
+
+
+
+
+
+
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
@@ -91,21 +537,7 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
 
 class SampleSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
