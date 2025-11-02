5f47c452e8c50ef4c04a354b0f79192cd5e14835
Anareaty <reatyrabota@yandex.ru>
2025-11-03 00:54:03 +0400

    check if embed is in popover (to prevent bug in older version)

--- src/patches/patchHoverPopover.ts
+++ src/patches/patchHoverPopover.ts
@@ -8,24 +8,26 @@ export const patchHoverPopover = async (plugin: PrettyPropertiesPlugin) => {
   plugin.patches.uninstallPPPopoverPatch = around(HoverPopover.prototype, {
     load(old) {
       return dedupe("pp-patch-popover-show-around-key", old, function(...args) {
-        let popover = this
-        updateImagesInPopover(popover, plugin)
+        if (this.embed) {
+          let popover = this
+          updateImagesInPopover(popover, plugin)
 
-        this.embed.previewMode.onRenderComplete = new Proxy(this.embed.previewMode.onRenderComplete, {
-          apply(old2, thisArg2, args2) {
-            updateImagesInPopover(popover, plugin)
-            return old2.call(thisArg2, ...args2)
-          }
-        })
+          this.embed.previewMode.onRenderComplete = new Proxy(this.embed.previewMode.onRenderComplete, {
+            apply(old2, thisArg2, args2) {
+              updateImagesInPopover(popover, plugin)
+              return old2.call(thisArg2, ...args2)
+            }
+          })
 
-        this.embed.showEditor = new Proxy(this.embed.showEditor, {
-          apply(old2, thisArg2, args2) {
-            let result = old2.call(thisArg2, ...args2)
-            updateImagesInPopover(popover, plugin)
-            return result
-          }
-        })
-
+          this.embed.showEditor = new Proxy(this.embed.showEditor, {
+            apply(old2, thisArg2, args2) {
+              let result = old2.call(thisArg2, ...args2)
+              updateImagesInPopover(popover, plugin)
+              return result
+            }
+          })
+        }
+      
         return old && old.apply(this, args)
       })
     }   
