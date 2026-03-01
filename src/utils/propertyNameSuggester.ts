28b589dfee0c08f32259e23660b0318e84368583
Nito <nikolas1997@hotmail.de>
2026-03-01 11:55:08 +0100

    restrict suggester to allowed properties

--- src/utils/propertyNameSuggester.ts
+++ src/utils/propertyNameSuggester.ts
@@ -1,25 +1,37 @@
 import { AbstractInputSuggest } from "obsidian";
 
+type PropertyInfo = { name: string; widget: string; };
+
 export class PropertyNameSuggest extends AbstractInputSuggest<string> {
+	private allowedTypes?: Set<string>;
+
+	constructor(app: any, inputEl: HTMLInputElement, allowedTypes?: Iterable<string>) {
+		super(app, inputEl);
+		if (allowedTypes)
+			this.allowedTypes = new Set(allowedTypes);
+	}
+
 	protected  getSuggestions(query: string): string[] {
 		const querySanitized = query.trim().toLowerCase();
-		const allPropertyNames = this.getAllPropertyNames();
+		const allAllowedPropertyNames = this.getAllPropertyNamesRestricted(this.allowedTypes);
 
 		if (!querySanitized)
-			return allPropertyNames;
+			return allAllowedPropertyNames;
 
-		return allPropertyNames.filter((p) => p.toLowerCase().includes(querySanitized));
+		return allAllowedPropertyNames.filter((p) => p.toLowerCase().includes(querySanitized));
 	}
 
 	renderSuggestion(value: string, el: HTMLElement): void {
 		el.setText(value);
 	}
 
-	private getAllPropertyNames(): string[] {
+	private getAllPropertyNamesRestricted(allowedTypes: Set<string> | undefined): string[] {
 		const cacheAny = this.app.metadataCache as any;
 		if (typeof cacheAny.getAllPropertyInfos === "function") {
-			const infos = cacheAny.getAllPropertyInfos() as Record<string,{ name?: string }>;
+			const infos = cacheAny.getAllPropertyInfos() as Record<string,PropertyInfo>;
+			console.log(infos);
 			const names = Object.values(infos)
+				.filter((p): p is PropertyInfo => !allowedTypes || allowedTypes.has(p?.widget))
 				.map((p) => p?.name)
 				.filter((n): n is string => typeof n === "string" && n.length > 0);
 
